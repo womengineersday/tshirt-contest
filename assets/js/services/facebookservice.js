a3ecd67bbe64b5b0427bc04e3c246f00c40f4885
Josh Willis <willis2342@gmail.com>
2015-02-04 23:37:58 -0500

    Fixed a bug I made

--- assets/js/services/facebookservice.js
+++ assets/js/services/facebookservice.js
@@ -7,20 +7,19 @@ function($rootScope, $location, $http) {
     getImages : function() {
 
       	FB.api('/430002157148842/photos',  function(resp) {
-            alert(resp);
             console.log(resp);
         });
 
 
-		// var image = {
-		// 	"thumbnailSource" : "http://placehold.it/300x300",
-		// 	"fullSource" : "http://placehold.it/500x500",
-		// 	"datePosted" : Date.today().toString("yyyy-MM-dd"),
-		// 	"likes" : "50",
-		// 	"shares" : "20"		//if you can get that
-		// }
-		// var images = [];
-		// for(var i = 0; i < 20; i++) images.push(image);
+		var image = {
+			"thumbnailSource" : "http://placehold.it/300x300",
+			"fullSource" : "http://placehold.it/500x500",
+			"datePosted" : Date.today().toString("yyyy-MM-dd"),
+			"likes" : "50",
+			"shares" : "20"		//if you can get that
+		}
+		var images = [];
+		for(var i = 0; i < 20; i++) images.push(image);
 
 		return images;
 
