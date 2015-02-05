acb28ec8b5065e382d591d29f7221569fb66c1f5
Josh Willis <willis2342@gmail.com>
2015-02-04 23:45:19 -0500

    Trying to get the auth response

--- assets/js/services/facebookservice.js
+++ assets/js/services/facebookservice.js
@@ -6,6 +6,26 @@ function($rootScope, $location, $http) {
 
     getImages : function() {
 
+        FB.getLoginStatus(function(response) {
+          if (response.status === 'connected') {
+            // the user is logged in and has authenticated your
+            // app, and response.authResponse supplies
+            // the user's ID, a valid access token, a signed
+            // request, and the time the access token 
+            // and signed request each expire
+            var uid = response.authResponse.userID;
+            var accessToken = response.authResponse.accessToken;
+
+            console(response.authResponse);
+
+          } else if (response.status === 'not_authorized') {
+            // the user is logged in to Facebook, 
+            // but has not authenticated your app
+          } else {
+            // the user isn't logged in to Facebook.
+          }
+        });
+
       	FB.api('/430002157148842/photos',  function(resp) {
             console.log(resp);
         });
