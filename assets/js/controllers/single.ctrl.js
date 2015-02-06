3d1fd8c1c1a47ee6b5a0b31da9deb168237465a6
Alex Klibisz <aklibisz@utk.edu>
2015-02-06 14:52:13 -0500

    testing

--- assets/js/controllers/single.ctrl.js
+++ assets/js/controllers/single.ctrl.js
@@ -1,11 +1,6 @@
 tc.controller('SingleController', ['$scope', '$location', 'ImgurService', '$routeParams',
   function($scope, $location, ImgurService, $routeParams) {
-
-    $scope.current_title = 'Test';
-    $scope.current_description = 'Test description';
-	$scope.socialURL = $location.absUrl();
-    $scope.testURL = $location.absUrl();
-    
+ 
     $scope.social = {
     	url : $location.absUrl(),
     	title : 'Check out this submission for the #WomEngineersDay T-Shirt Contest',
@@ -18,10 +13,6 @@ tc.controller('SingleController', ['$scope', '$location', 'ImgurService', '$rout
       console.log("SingleController: calling ImgurService service to get image with ID: " + $routeParams.id);
       $scope.image = result;
     });
-
     
-
-
-
   }
 ]); //  SingleController
