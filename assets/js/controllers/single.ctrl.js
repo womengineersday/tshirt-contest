6f96aa797e3f5456fa76025666443d3f2996f574
Alex Klibisz <aklibisz@utk.edu>
2015-02-06 17:56:23 -0500

    back to regular buttons

--- assets/js/controllers/single.ctrl.js
+++ assets/js/controllers/single.ctrl.js
@@ -1,16 +1,6 @@
 tc.controller('SingleController', ['$scope', '$location', 'ImgurService', '$routeParams',
   function($scope, $location, ImgurService, $routeParams) {
  
-    $scope.social = {
-    	url : 'http://goo.gl/BEQdZF',
-    	//url : $location.absUrl(),
-    	//url2 : window.location.href,
-    	fburl : encodeURIComponent(encodeURIComponent($location.absUrl())),
-
-    	title : 'Check out this submission for the #WomEngineersDay T-Shirt Contest',
-    	description : 'Check out this submission for the #WomEngineersDay T-Shirt Contest'
-    }
-
     $scope.image = {};
 
     ImgurService.getImage($routeParams.id).then(function(result) {
@@ -18,14 +8,46 @@ tc.controller('SingleController', ['$scope', '$location', 'ImgurService', '$rout
       $scope.image = result;
     });
 
-    $scope.myModel = {
-	  Name: "Check out this submission for the #WomEngineersDay T-Shirt Contest!",
-	  ImageUrl: 'http://www.jasonwatmore.com/pics/jason-watmore.jpg',
-	  FbLikeUrl: encodeURIComponent(encodeURIComponent($location.absUrl()))
-	  //FbLikeUrl: 'http://jasonwatmore.com/post/2014/08/01/AngularJS-directives-for-social-sharing-buttons-Facebook-Like-GooglePlus-Twitter-and-Pinterest.aspx'
-	};
+ //    $scope.social = {
+ //    	url : 'http://imgur.com/' + $routeParams.id,
+ //    	//url : $location.absUrl(),
+ //    	//url2 : window.location.href,
+ //    	fburl : encodeURIComponent(encodeURIComponent($location.absUrl())),
 
-    console.log($scope.social.url);
+ //    	title : 'Check out this submission for the #WomEngineersDay T-Shirt Contest',
+ //    	description : 'Check out this submission for the #WomEngineersDay T-Shirt Contest'
+ //    }
+
+ //    $scope.myModel = {
+	//   Name: "Check out this submission for the #WomEngineersDay T-Shirt Contest!",
+	//   ImageUrl: 'http://www.jasonwatmore.com/pics/jason-watmore.jpg',
+	//   FbLikeUrl: encodeURIComponent(encodeURIComponent($location.absUrl()))
+	//   //FbLikeUrl: 'http://jasonwatmore.com/post/2014/08/01/AngularJS-directives-for-social-sharing-buttons-Facebook-Like-GooglePlus-Twitter-and-Pinterest.aspx'
+	// };
+
+ //    console.log($scope.social.url);
+
+ 	$scope.social = {
+ 		fbURL : 'http://imgur.com/' + $routeParams.id
+ 	}
+
+    $scope.smbInit = function() {
+      try{
+        console.log("reloading FB buttons");
+        FB.XFBML.parse();
+      }catch (ex) { }    
+      try {
+        console.log("reloading Twitter button");
+        twttr.widgets.load();  
+      } catch(ex) { }
+      try {
+        console.log("reloading Google+ button");
+        gapi.plusone.go();
+      } catch(ex) { }
+    };
+
+
+    //$scope.smbInit();
 
   }
 ]); //  SingleController
