44de56fd9bac2797b93c2fc01af3cf1736196870
Alex Klibisz <aklibisz@utk.edu>
2015-02-06 19:04:43 -0500

    added watch

--- assets/js/controllers/single.ctrl.js
+++ assets/js/controllers/single.ctrl.js
@@ -1,6 +1,7 @@
 tc.controller('SingleController', ['$scope', '$location', 'ImgurService', '$routeParams',
 	function($scope, $location, ImgurService, $routeParams) {
 
+		$scope.baseURL = 'http://womengineersday.com/tshirts/#/s/';
 		$scope.image = {};
 
 		ImgurService.getImage($routeParams.id).then(function(result) {
@@ -9,11 +10,24 @@ tc.controller('SingleController', ['$scope', '$location', 'ImgurService', '$rout
 			console.log("SingleController: image object loaded with URL: " + $scope.image.fullSource);
 		});
 
+		/*
+			re-initialize the social media buttons once the image and
+			it's URL have been retrieved
+		*/
 		$scope.$watch('image', function(newVal, oldVal) {
 			console.log('SingleController: image object changed: ', $scope.image);
 			try {
 				console.log("reloading FB buttons");
 				FB.XFBML.parse();
+			} catch (ex) {}
+			try {
+				console.log("reloading Twitter button");
+				twttr.widgets.load();
+			} catch (ex) {}
+
+			try {
+				console.log("reloading Google+ button");
+				gapi.plusone.go();
 			} catch (ex) {}
 		})
 
